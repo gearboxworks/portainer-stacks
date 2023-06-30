1e2d87fbb252f8206d09df8fe6636b790f9676d9
Mike Schinkel <mike@newclarity.net>
2023-06-30 04:48:32 -0400

    Mostly feature complete traefik-mdns script

--- root/usr/local/bin/ensure-traefik-routes-in-mdns.sh
+++ root/usr/local/bin/ensure-traefik-routes-in-mdns.sh
@@ -10,7 +10,7 @@ DEBUG=0
 function debug_msg {
   local msg="$1"
   if [ $DEBUG -eq 1 ] ; then
-    printf "%s" "${msg}"
+    printf "\n%s" "${msg}"
   fi
 }
 
@@ -21,12 +21,71 @@ function publish_domain {
   /usr/bin/avahi-publish -a "${domain}" -R "${host_ip}" &
 }
 
-function get_cached {
-  if ! [ -f "${CACHE_FILE}" ] ; then
-    echo '{}' > "${CACHE_FILE}"
+function read_cached_json {
+  local cached_json=""
+
+  if [ -f "${CACHE_FILE}" ] ; then
+    cached_json="$(cat "${CACHE_FILE}")"
   fi
-  < "${CACHE_FILE}" \
-    jq '.[].domains.name' | sort | tr "\n" ' '
+
+  if [ "" != "${cached_json}" ] ; then
+    echo "${cached_json}"
+  else
+    echo '{"domains":[]}' | sudo tee "${CACHE_FILE}"
+    return
+  fi
+}
+
+function write_cached_json {
+  local cached_json="$1"
+  printf "%s" "${cached_json}" | sudo tee "${CACHE_FILE}" >/dev/null
+}
+
+function extract_cached_domains {
+  local cached_json="$1"
+  if [ "${cached_json}" == "" ] ; then
+    cached_json="$(read_cached_json)"
+  fi
+  jq -r '.domains[]|.name' <<< "${cached_json}" \
+    | sort \
+    | tr "\n" ' '
+}
+
+# Dedups a space-separated list of domains
+function dedup_domains {
+  local domains="$1"
+  tr ' ' "\n" <<< "${domains}" \
+    | sort \
+    | uniq \
+    | tr "\n" ' '
+}
+
+function cache_has_domain {
+  local domain="$1"
+  local cached_domains="$2"
+  local query
+
+  query="$(printf '.domains[]|select(.name=="%s")' "${domain}")"
+  result="$(jq -r "${query}" <<< "${cached_domains}")"
+  test "" != "${result}"
+}
+
+function add_domain {
+  local domain="$1"
+  local cached_domains="$2"
+  local query
+
+  query="$(printf '.domains += [{"name":"%s", "last_seen": "%s"}]' "${domain}" "$(current_iso8601_datetime)")"
+  jq "${query}" <<< "${cached_domains}"
+}
+
+function update_domain {
+  local domain="$1"
+  local cached_domains="$2"
+  local query
+
+  query="$(printf '.domains |= map(if .name == "%s" then .last_seen = "%s" else . end)' "${domain}" "$(current_iso8601_datetime)")"
+  jq "${query}" <<< "${cached_domains}"
 }
 
 function current_iso8601_datetime {
@@ -34,11 +93,11 @@ function current_iso8601_datetime {
   date -u +"%Y-%m-%dT%H:%M:%SZ"
 }
 
-function get_routers {
+function retrieve_routers_json {
   curl --silent "${TRAEFIK_ROUTERS_API_URL}"
 }
 
-function get_published {
+function discover_published_domains {
   pgrep -a avahi-publish | awk '{print $4}' | tr "\n" ' '
 }
 
@@ -46,12 +105,11 @@ function kill_published {
   pgrep avahi-publish | xargs kill -9
 }
 
-function get_defined {
-  local json="$1"
+#
+function extract_retrieved_domains {
+  local routers_json="$1"
 
-  jq --null-input \
-    --argjson routers "${json}" \
-    '$routers | .[] | select(.rule|contains("Host(`"))|.rule' \
+  jq -e '.[] | select(.rule|contains("Host(`"))|.rule' <<< "${routers_json}" \
       | tr '`' ' ' \
       | awk '{print$2}' \
       | sort \
@@ -59,41 +117,91 @@ function get_defined {
       | tr "\n" ' '
 }
 
+# Timing out domains last seen more than 24 hours ago.
+function timeout_domains {
+  local timeout_hours="$1"
+  local cached_json="$2"
+  local map
+  local query
+  local yesterday_seconds
+
+  yesterday_seconds="$(( $(date +%s) - 60*60*timeout_hours ))"
+  map="$(printf 'map(select((.last_seen | strptime("%s") | mktime)' "%Y-%m-%dT%H:%M:%SZ")"
+  query="$(printf '.domains |= %s > %d))' "${map}" "${yesterday_seconds}")"
+  jq "${query}" <<< "${cached_json}"
+}
+
 function main() {
   local space=' '
   local updated=0
-  local routers
-  local published
-  local cached
-  local defined
+  local routers_json
+  local published_domains
+  local cached_json
+  local cached_domains
+  local retrieved_domains
+  local potential_domains
   local domain
 
+  debug_msg "Read the cache file to find any domains that were previously there."
+  cached_json="$(read_cached_json)"
 
-  cached="$(get_cached)"
-  routers="$(get_routers) ${cached}"
-  if [ "${routers}" == "" ] ; then
-    printf "\nWarning: Unable to reach %s" "${TRAEFIK_ROUTERS_API_URL}" >&2
-    # TODO Load cached values from JSON file
-    #      Omit those last seen more than 24 hour ago
+  debug_msg "Time out domains last seen more than 24 hours ago."
+  cached_json="$(timeout_domains 24 "${cached_json}")"
+
+  debug_msg "Extract cached domains from cached json that was read from the cache file."
+  cached_domains=$(extract_cached_domains "${cached_json}")
+
+  debug_msg "Send an HTTP GET request to retrieve the routers JSON from Traefik API."
+  routers_json="$(retrieve_routers_json)"
+  if [ "${routers_json}" == "" ] ; then
+    printf 'Warning: Unable to reach %s' "${TRAEFIK_ROUTERS_API_URL}" >&2
   fi
-  published="${space}$(get_published)${space}"
-  defined="$(get_defined "${routers}")"
-  for domain in $defined; do
-    debug_msg "$(printf "\nChecking if %s is published..." "${domain}")"
-    # Loop through $defined and if not in $published then publish
-    if [[ "${published}" =~ ${space}${domain}${space} ]] ; then
-      # As it is published, look to the next one
-      # Update JSON with last seen time
-      debug_msg "already published."
+
+  debug_msg "Extract just the .local domains retrieved in Traefik into a space-separated string."
+  retrieved_domains="$(extract_retrieved_domains "${routers_json}")"
+
+  debug_msg "Combine Traefik-defined domains an previously cached domains into potential domains."
+  potential_domains="${cached_domains} ${retrieved_domains}"
+
+  debug_msg "Deduplicate domains after combining Traefik-defined and previously cached domains."
+  potential_domains="$(dedup_domains "${potential_domains}")"
+
+  debug_msg "Use pgrep to discover the avahi-published .local domains from list of processes."
+  published_domains="$(discover_published_domains)"
+
+  debug_msg "Loop through the domains retrieved from Traefik to see if they have been published."
+  for domain in $potential_domains; do
+    debug_msg "Attempt to match a .local domain retrieved from Trafik to the list of domains currently published."
+    debug_msg "$(printf 'Checking if %s is published_domains...' "${domain}")"
+    if [[ "${space}${published_domains}${space}" =~ ${space}${domain}${space} ]] ; then
+      [ $DEBUG -eq 1 ] && printf "already published"
+      debug_msg "$(printf 'Updating .last_seen %s in cache file %s.' "${domain}", "${CACHE_FILE}")"
+      debug_msg 'Since the domain is already published update JSON with last seen time.'
+      if cache_has_domain "${domain}" "${cached_json}" ; then
+        cached_json="$(update_domain "${domain}" "${cached_json}")"
+      else
+        cached_json="$(add_domain "${domain}" "${cached_json}")"
+      fi
+      debug_msg 'Now look at the next domain.'
       continue
     fi
-    printf "\nPublishing %s" "${domain}"
+    printf 'Publishing %s' "${domain}"
     if ! publish_domain "${HOST_IP_ADDRESS}" "${domain}"; then
-      printf "\nError: Unable to publish %s" "${domain}" >&2
+      printf 'Error: Unable to publish %s' "${domain}" >&2
     fi
+    debug_msg "$(printf 'Adding %s to cache file %s.' "${domain}" "${CACHE_FILE}")"
+    cached_json="$(add_domain "${domain}" "${cached_json}")"
     updated=1
-    # Write updated JSON to CACHE_FILE
+    debug_msg "Write updated JSON to CACHE_FILE"
   done
+  # TODO REMOVE PUBLISHED DOMAINS NOT IN TRAEFIK
+  if [ "" != "${retrieved_domains}" ]; then
+    debug_msg "Writing cache."
+    write_cached_json "${cached_json}"
+  fi
+  if [ $DEBUG -eq 1 ]; then
+    printf "\n"
+  fi
   if [ $updated -eq 1 ]; then
     printf "\n"
   fi
