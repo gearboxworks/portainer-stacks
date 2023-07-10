916775d9c536a0353df691863b43ba647e7775b4
Mike Schinkel <mike@newclarity.net>
2023-07-10 02:46:29 -0400

    Script to install HTTPIE, for future reference

---
+++ provision/install-httpie.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install httpie
+curl -SsL https://packages.httpie.io/deb/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/httpie.gpg
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/httpie.gpg] https://packages.httpie.io/deb ./" > /etc/apt/sources.list.d/httpie.list
+sudo apt update
+sudo apt install httpie
