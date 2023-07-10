3243408693fb7b297ca6d6059db39e37c4580bc8
Mike Schinkel <mike@newclarity.net>
2023-07-10 04:20:05 -0400

    Fix mdns publishing service
    - Needed to use `Type=forking`
    - Also renamed script
    - Added scripts to list and kill published .local domains

--- provision/usr/local/bin/perms.sh
+++ provision/usr/local/bin/perms.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source /tmp/provision/shared.sh
 
-sudo_chmod +x ensure-traefik-routes-in-mdns.sh
-sudo_chmod 755 ensure-traefik-routes-in-mdns.sh
+sudo_chmod +x publish-traefik-mdns.sh
+sudo_chmod 755 publish-traefik-mdns.sh
