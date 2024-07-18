dbd3a9568b23657f48f285793cc8a501dd315955
Mike Schinkel <mike@newclarity.net>
2024-07-17 21:02:44 -0400

    Trying a Statamic config

--- files/statamic/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -e
-if [ ! -L /var/www/html/storage ]; then
-  rm -rf /var/www/html/storage
-  ln -s /var/statamic/storage /var/www/html/storage
-fi
-exec supervisord -c /etc/supervisor/supervisord.conf
