a5f9fe725cdd0fb1052698e8f2db74e1abb3ec3b
Mike Schinkel <mike@newclarity.net>
2024-07-17 21:34:45 -0400

    Restore image delete in `build.sh`

--- files/statamic/build.sh
+++ files/statamic/build.sh
@@ -5,7 +5,6 @@ for c in $(docker ps --filter "ancestor=${image}" -q) ; do
   docker stop "${c}" && docker rm "${c}"
 done
 
-#Maybe I don't need this next line?
-#docker rmi "$(docker images | grep statamic | awk '{print $3}')"
+docker rmi "$(docker images | grep statamic | awk '{print $3}')"
 
 docker build -t statamic:latest -f Dockerfile.statamic .
